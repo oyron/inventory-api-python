0aecf75f9b1ee872b682509009481bf38647fcc6
Øyvind Rønne <oyron@equinor.com>
2021-01-26 12:31:52 +0100

    First take

---
+++ flaskr/book_inventory.py
@@ -0,0 +1,46 @@
+class BookInventory:
+
+    class Book:
+        def __init__(self, uid, title, author):
+            self.uid = uid
+            self.title = title
+            self.author = author
+    
+
+    def __init__(self):
+        self.uid_sequence = 0
+        self.books = {}
+        self.add_book('1984', 'George Orwell')
+        self.add_book('War and Peace', 'Leo Tolstoy')
+        self.add_book('Robinson Crusoe', 'Daniel Defoe')
+
+
+
+    def get_all_books(self):
+        return list(self.books.values())
+
+    def get_book(self, book_id):
+        return self.books.get(str(book_id))
+
+    def add_book(self, title, author):
+        uid = self.uid_sequence
+        self.uid_sequence += 1
+        return self._add_book(uid, title, author)
+
+    def update_book(self, book_id, title, author):
+        return self._add_book(book_id, title, author)
+
+    def has_book(self, book_id):
+        return str(book_id) in self.books
+
+    def delete_book(self, book_id):
+        self.books.pop(str(book_id))
+
+    def _add_book(self, book_id, title, author):
+        book = BookInventory.Book(int(book_id), title, author)
+        self.books[str(book_id)] = book
+        return book
+
+    
+
+
---
+++ tests/test_book_inventory.py
@@ -0,0 +1,68 @@
+import unittest
+from context import flaskr
+from flaskr.book_inventory import BookInventory
+
+
+class TestBookInventory(unittest.TestCase):
+
+
+
+    def test_get_all_books(self):
+        book_inventory = BookInventory()
+        books = book_inventory.get_all_books()
+        self.assertIsInstance(books, list)
+    
+    def test_get_book_by_string_id(self):
+        book_inventory = BookInventory()
+        book = book_inventory.get_book("1")
+        self.assertIsNotNone(book)
+        self.assertTrue(type(book.uid) == int)
+        self.assertTrue(type(book.author) == str)
+
+    def test_get_book_by_numeric_id(self):
+        book_inventory = BookInventory()
+        book = book_inventory.get_book(1)
+        self.assertIsNotNone(book)
+        self.assertTrue(type(book.uid) == int)
+        self.assertTrue(type(book.author) == str)
+
+    def test_add_book(self):
+        title = "The Statoil Book"
+        author = "Eldar Sætre"
+        book_inventory = BookInventory()
+        book = book_inventory.add_book(title, author)
+        self.assertIsNotNone(book)
+        self.assertEqual(book.title, title)
+        self.assertEqual(book.author, author)
+
+    def test_update_book_by_numeric_id(self):
+        title = "The Statoil Book"
+        author = "Eldar Sætre"
+        book_id = 1
+        book_inventory = BookInventory()
+        book = book_inventory.update_book(book_id, title, author)
+        self.assertIsNotNone(book)
+        self.assertEqual(book.title, title)
+        self.assertEqual(book.author, author)
+        self.assertEqual(book.uid, book_id)
+
+    def test_update_book_by_string_id(self):
+        title = "The Statoil Book"
+        author = "Eldar Sætre"
+        book_id = 1
+        book_inventory = BookInventory()
+        book = book_inventory.update_book(str(book_id), title, author)
+        self.assertIsNotNone(book)
+        self.assertEqual(book.title, title)
+        self.assertEqual(book.author, author)
+        self.assertEqual(book.uid, book_id)
+
+    def test_delete_book_by_string_id(self):
+        book_id = 1
+        book_inventory = BookInventory()
+        self.assertTrue(book_inventory.has_book(book_id))
+        book_inventory.delete_book(book_id)
+        self.assertTrue(not book_inventory.has_book(book_id))
+
+if __name__ == '__main__':
+    unittest.main()
